9a70ab4272f97e9fe910a5a9e1fc41c0097015ed
Vishal Dedania <vdedania@createthegroup.com>
2015-11-09 11:09:39 -0500

    Adding product categories association controller and models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151108171633) do
+ActiveRecord::Schema.define(version: 20151109160629) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
